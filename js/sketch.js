68e27cf62dd93d5761d9619e7558bf3eb76412fc
tolaoniyangi <s.i.oniyangi@gmail.com>
2017-02-27 20:49:16 -0500

    did Drawing with Data classwork

--- js/sketch.js
+++ js/sketch.js
@@ -1,62 +1,123 @@
-// command+backslash to comment or just type two backslashes
-//build function, call it in setup 
-//very similar to Python
+// we are going to create two dummy arrays with some data (one for values and another one for country names), 
+// and we will create a bar chart with this data
 
-//numbers and booleans don't need quotes, string does
+// ***** Global variables ***** //
+var myData = [12, 43, 15, 25, 34];
+var labels = ['Colombia', 'Peru', 'Venezuela', 'Chile', 'Argentina'];
 
-var myFirstVariable = 20;
-var secondVariable = 35;
-var myName = "tola's car";
-var amIamerican = false; //boolean
-var numbers = [12, 214, 523, 625] //array, needs square bracket
-var myObject = {name: "tola", lastName: "Oniyangi", age: 21, nigerian: true}//object, use consistent quotes either double or single
-var x5= "not mystery value" //global variable
-var circleSize = 40;
+// ***** Setup function ***** //
+function setup(){
+  createCanvas(800, 800);
+  textAlign(LEFT, TOP);
+  textSize(18);
+  console.log('Setup complete...');
+}
 
-//Javascript is asynchronous i.e. it runs all the functions at the same time - the preload function forces it to run whatever first 
-// function preload(){ 
+// ***** Draw function ***** //
+function draw(){
+  background(255);
+  fill(0);
+  noStroke();
+  // Drawing the bar chart
+  for (var i = 0; i < myData.length; i++) {
+    rect(100, 5 + 20*i, myData[i] * 10, 15);
+  }
+  // Drawing the labels
+  for (var i = 0; i < labels.length; i++) {
+    text(labels[i], 10, 20*i);
+  }
+}
 
+// load the data and create a bar chart with it//
+// ***** Global variables ***** //
+var refugeeTable;
+var topRefugeesTable = new p5.Table;
+var maxTotal = 0;
+var maxLabel = 0;
+var maxLength = 550;
+
+// ***** Preload function ***** //
+function preload(){
+  refugeeTable = loadTable('../data/RefugeesUNHCR.csv', 'csv', 'header');
+  console.log('Done loading table...');
+}
+
+// ***** Setup function ***** //
+function setup(){
+  createCanvas(800, 3000);
+  textSize(12);
+  console.log('Setup complete...');
+  print(refugeeTable.getRowCount() + ' rows loaded...');
+  print(refugeeTable.getColumnCount() + ' columns loaded...');
+  for (var i = 0; i < refugeeTable.getRowCount(); i++) {
+    maxTotal = max(refugeeTable.getNum(i, 'Total'), maxTotal);
+    maxLabel = max(refugeeTable.getString(i, 'Country').length, maxLabel);
+  }
+  print('Maximum total is ' + maxTotal);
+  print('Maximum label length is ' + maxLabel);
+  createNewTable();
+}
+
+
+// ****** Create new table function ******** //
+function createNewTable(){
+    topRefugeesTable.addColumn('Country');
+    topRefugeesTable.addColumn('Total');
+    for (var i = 0; i < refugeeTable.getRowCount(); i++) {
+        var totalRefugees = refugeeTable.getNum(i, 'Total');
+        if (totalRefugees >= 100000) {
+            var newRow = topRefugeesTable.addRow()
+            newRow.setString('Country', refugeeTable.getString(i, 'Country'));
+            newRow.setNum('Total', refugeeTable.getNum(i, 'Total'));
+        }
+    }
+    print('New top refugee table created...');
+}
+
+function drawCountries(tableName){
+    fill(0);
+    noStroke();
+    textAlign(LEFT, TOP);
+    for (var i = 0; i < tableName.getRowCount(); i++) {
+        var total = tableName.getNum(i, 'Total');
+        var length = map(total, 0, maxTotal, 0, maxLength);
+        rect(maxLabel * 5, 2 + 14*i, length, 12);
+        text(nfc(total, 0), maxLabel * 5 + length + 5, 14*i);
+    }
+    textAlign(RIGHT, TOP);
+    for (var i = 0; i < tableName.getRowCount(); i++) {
+        text(tableName.getString(i, 'Country'), maxLabel * 5 - 5, 14*i);
+    }
+}
+
+// ***** Draw function ***** //
+function draw(){
+    background(255);
+    // drawCountries(refugeeTable);
+    drawCountries(topRefugeesTable);
+}
+
+// ***** Draw function --> basically for every instance of the
+// number i, which is first called as 0, and then defined as  
+// less than the row count i.e. whenever row count >0, and i++
+// runs after the loop to move onto the next instance of i
+// (i.e. increases a value after the loop has been executed) ***** //
+// function draw(){
+//     background(255);
+//     fill(0);
+//     noStroke();
+//     textAlign(LEFT, TOP);
+//     for (var i = 0; i < refugeeTable.getRowCount(); i++) {
+//         var total = refugeeTable.getNum(i, 'Total');
+//         var length = map(total, 0, maxTotal, 0, maxLength);
+//         rect(maxLabel * 5, 2 + 14*i, length, 12);
+//           // rect(x,y,width,height,radius [if rounded rectangle])
+//         text(nfc(total, 0), maxLabel * 5 + length + 5, 14*i);
+//     }
+//     textAlign(RIGHT, TOP);
+//     for (var i = 0; i < refugeeTable.getRowCount(); i++) {
+//         text(refugeeTable.getString(i, 'Country'), maxLabel * 5 - 5, 14*i);
+//           // text(content/string, x, height)
+//     }
 // }
 
-function myFirstFunction (x1,x2,x3){ //defines the function
-	var x4= x1*x2*x3; //gonna multiply the three variables
-	var x5='mysteryvalue'; //local variable, cannot be called in another function
-	return x4; //does not print
-}
-
-function setup(){
-	// console.log(myFirstVariable); // console log prints to my console
-	// print(myFirstVariable); // prints to my console
-	// var thirdVariable = myFirstVariable + secondVariable; --> this adds the numbers to 35
-	//var thirdVariable = myFirstVariable + myName +secondVariable; // appends my name to it and treats as text (everything as text if before addition of numbers), treats them in order 
-	//print(thirdVariable);
-	//print(typeof(thirdVariable));
-	//print(numbers[0]); //to get the first number out of the array (first position)
-	//print(myObject["name"]); //how to print something from an object, use consistent quotes either double or single
-	//print(myFirstFunction(2,4,5)); //calls the function
-	createCanvas(500,500); //creating drawing space of 500 by 500
-	// for (var i=0; i<100; i++){ //loop: for variabe i that equals 0, when i <100 do a couple of things ie add 1 to i and then prints it --at the end it prints 0 up to 99
-	// 	print(i);
-	// }
-	var i=0;
-	while (i<100){ //exactly same as loop above but different way of writing it
-		print(i);
-		i++
-	}
-} 
-
-
-function draw (){
-	// print(myFirstVariable); //prints to my console many times
-	// background(0, 100, 100); //gives background in rgb
-	// if (mouseIsPressed){
-	// 	fill(250);
-	// }
-	// else{
-	// 	fill(0);
-	// }
-	// stroke(200,0,50);
-	// strokeWeight(5);
-	// //ellipse(mouseX, mouseY, 20, 20); //takes four values - x is wherever my mouse is, y is wherever my mouse is, width, height
-	// ellipse(mouseX, mouseY, circleSize, circleSize);
-}
