add2a0e65f511dfdbc7edfbcbad7a1a728be35e8
tolaoniyangi <s.i.oniyangi@gmail.com>
2017-03-20 08:38:14 -0400

    fixed cell that wasn't in loop by adding noLoop(); drew the rainbow image with increasing saturation towards bottom successfully

--- js/Exercise01.js
+++ js/Exercise01.js
@@ -1,20 +1,20 @@
 function setup(){
 	createCanvas(400, 400);
 	colorMode(HSB,1);
+	noLoop(); //The noLoop() function causes draw() to only execute once. Without calling noLoop(), the code inside draw() is run continually.
 }
 
 function draw(){
-	background(0);
 	noStroke();
 	var y = 10;
 	for (y = 0; y < height ; y+=10) 
 	for (x = 0; x < width ; x+=10) {
 		rect(x,y,10,10); //draws the recurring rectangles based on x & y that were defined earlier 
-	 //does the fill to be a gradient rainbow 
+	 	//does the fill of the rectang;es to be a gradient rainbow 
 	  	var ratio = x/width;
 	  	var hue = ratio;
-	 	for (var i = y; i <= height; i++) {
-		    var saturation = map(i, y, y+height, 1, 0); //figure out why the cell in the top left corner is going to black
+	 	for (var i = y ; i <= height; i++) {
+		    var saturation = map(i, y, y+height, 1, 0); //why the cell in the top left corner was not being affected by the loop until i added no loop because it was drawing continuously/restarting
 	    }
 		var brightness = 1;
 	  	fill(hue, saturation, brightness);
