5cd54ceb4e8280552d8f2f755e8468e6cceff8d0
tolaoniyangi <s.i.oniyangi@gmail.com>
2017-03-22 23:59:26 -0400

    sorted out both color and size in relation to mouse, isolated the change in size/color to the canvas

--- js/Exercise02.js
+++ js/Exercise02.js
@@ -1,26 +1,43 @@
+function setup(){
+	colorMode(RGB,1);
+}
+
 function draw(){
 	createCanvas(400,400);
 	stroke('black');
 	strokeWeight(.5)
 
-	for (y = 0; y < height ; y+=17) //y+=25 refers to the spacing in between circles
-	for (x = 0; x < width ; x+=17) {
+	for (y = 0; y < height ; y+=19) //y+=25 refers to the spacing in between circles
+	for (x = 0; x < width ; x+=19) {
     
-    	//dist= the distance from point 1 to point 2.
-    	var d = int(dist(x, y, mouseX, mouseY)); //distance between the position of the mouse and the center of each circle
+    	for (var i = 0; i<100; i++) {
+    		if (mouseX<400 && mouseY<400 && mouseY>3){ //&&=and, ||=or
+     			var d = int(dist(x, y, mouseX, mouseY)); //distance from the center of each circle to the position of the mouse
+    		}
 
+    		else {
+    			var d = 200;
+    		}
+    	};
+  		
+  		//setting up
 	    var calcRadius;
 	    for (var i = 0; i < 100; i++) {    		
-	    	if (d<800) {
-	    		calcRadius=d/20;
-	    	}
-	    	else {
-	    		calcRadius=5;
-	    	}
+	    	calcRadius=d/21;
 	    } 
-    // var calcColor = 
 
+    	var calcColor;
+    	for (var i = 0; i < 100; i++) {    		
+	    	if (mouseX<400 && mouseY<400 && mouseY>3){ //&&=and, ||=or
+	    		calcColor=(255,255,255,d/550);
+    		}
+    		else {
+    			calcColor=('rgba(255,100,20,0.9)');
+    		}
+	    } 
+
+	    //drawing it 
 		ellipse(x,y,calcRadius,calcRadius);
-		fill('gray');
+		fill(calcColor);
 	}
 }
