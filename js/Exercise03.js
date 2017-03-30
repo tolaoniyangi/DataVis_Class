a25c141305d7e6315cc441a61c02f8d4ad3c9007
tolaoniyangi <s.i.oniyangi@gmail.com>
2017-03-30 17:24:45 -0400

    Stuck for the past five hours tring to get the JSON to be parsed well - keeps coming up as undefined

--- js/Exercise03.js
+++ js/Exercise03.js
@@ -1,35 +1,49 @@
-//**** global variable ****//
-var stationInfoURL = 'https://gbfs.citibikenyc.com/gbfs/en/station_information.json';
-// var stationStatusURL = 'https://gbfs.citibikenyc.com/gbfs/en/station_status.json';
-var stationInfoData;
-var stationStatusData;
-// var button;
-var stationName;
-var getStationData;
 
-//**** Setup function ***//
-function setup(){ //load data in here
-    createCanvas(800,800);
-    // button = select ('#submit'); //# represents the id of the button 
-    // stationName = select ('#stationName');
-    // button.mousePressed(queryAPI); //linking queryAPI to the button 
-    var query = stationInfoURL + stationName; //x.value() lets it take whatever element in the value and link it
-    loadJSON(stationInfoURL, getStationData); //loadJSON (url, callback). getStationData is the callback function 
-}   //jsonp allows you to bypass the problem w https
+var name;
+var stationInfoURL;
+var station_id=72;
+var data;
+var stations;
 
-function getStationData(apiData){ //callback for loading the data /executing the function, apiData=variable that stores this new data 
-    stationData = apiData; //so stationdata now stores everything in API data 
-    // stationName = stationData.name;
-    // stationID = stationData.data.stations.station_id;
-    console.log(stationData);
-        background(0);
-        for (var i = 0; i < stationData.number; i++) {
-            fill(255);
-            ellipse(random(width), random(height), 19, 19)
-        };
+//setup
+function setup(){
+    // createCanvas(500,500);
+
+    //getting JSON citibike data via AJAX 
+    var stationInfoRequest=new XMLHttpRequest();
+    stationInfoRequest.open('GET', 'https://gbfs.citibikenyc.com/gbfs/en/station_information.json',true); //argument to recieve or download data
+    stationInfoRequest.onload=function(){
+        var stationInfoData=JSON.parse(stationInfoRequest.responseText); // so we've defined our request and parsed it so it treats it as objects not a blob of text+ saved it to a variable 
+        console.log(stationInfoData);
+        alert(obj.data[0].stations);
+    }
+    stationInfoRequest.send(); //sends the request 
+    
+    
+    // var stationStatusRequest=new XMLHttpRequest();
+    // stationStatusRequest.open('GET', 'https://gbfs.citibikenyc.com/gbfs/en/station_status.json');
+    // stationStatusRequest.onload=function(){
+    //     var stationStatusData=JSON.parse(stationStatusRequest.responseText);
+    //     console.log(stationStatusData);
+    // }
+    // stationStatusRequest.send();
+}
+
+function useJSON(setup){
+    console.log(stationInfoData)
 }
 
 // function queryAPI(){ //building the url and loading the json in this function 
-//     var query = stationInfoURL + stationName.value(); //city.value() lets it take whatever element in the value and link it
-//     loadJSON(query, getStationData); //once you've loaded the query, then do this: get weather data
+//     var query = stationInfoURL + data.stations.station_id.value(); //city.value() lets it take whatever element in the value and link it
+//     loadJSON(query, getWeatherData); //once you've loaded the query, then do this: get weather data
+// }
+
+// function draw(){
+//     background(255); //so it erases everything 
+//     fill(0);
+//     noStroke();
+//     if(stationStatusData){
+//         ellipse(20,20,station_id,station_id * 10)
+//     }
+
 // }
