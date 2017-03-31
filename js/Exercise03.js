8d4660a9062ee6cfc1aaf6790b4e072d43a76f5f
tolaoniyangi <s.i.oniyangi@gmail.com>
2017-03-30 21:27:10 -0400

    bare bones of weather app

--- js/Exercise03.js
+++ js/Exercise03.js
@@ -1,57 +1,40 @@
-var name;
-var stationInfoURL;
-var station_id=72;
-var data;
-var stations;
-var stationInfoData;
+//**** global variable ****//
+var apiKey = '7db2c379e5eccdbe63af88ebaa03ea48';
+var baseURL = 'http://api.openweathermap.org/data/2.5/weather?q=';
+var city;
+var units = 'metric';
+var weatherData; 
+var temperature = 0;
+var humidity = 0;
+var button;
 
-//setup
-function loadJSON(url,callback){
-    // createCanvas(500,500);
-
-    //getting JSON citibike data via AJAX 
-    var stationInfoRequest=new XMLHttpRequest();
-        stationInfoRequest.overrideMimeType("application/json");
-        stationInfoRequest.open('GET', 'https://gbfs.citibikenyc.com/gbfs/en/station_information.json',true); //argument to recieve or download data
-        stationInfoRequest.onreadystatechange=function(){
-        if (stationInfoRequest.readyState ==4 && stationInfoRequest.status == "200") {
-            callback(stationInfoRequest.responseText);
-        }
-    };
-    stationInfoRequest.send(null); //sends the request 
-    
-    
-    // var stationStatusRequest=new XMLHttpRequest();
-    // stationStatusRequest.open('GET', 'https://gbfs.citibikenyc.com/gbfs/en/station_status.json');
-    // stationStatusRequest.onload=function(){
-    //     var stationStatusData=JSON.parse(stationStatusRequest.responseText);
-    //     console.log(stationStatusData);
-    // }
-    // stationStatusRequest.send();
+//**** Setup function ***//
+function setup(){
+    createCanvas(800,800);
+    button = select ('#submit'); //# represents the id of the button 
+    city = select ('#city');
+    button.mousePressed(queryAPI); //linking queryAPI to the button 
 }
 
-function init(){
-    loadJSON('https://gbfs.citibikenyc.com/gbfs/en/station_information.json',function(response){
-        var stationInfoData=JSON.parse(response); // so we've defined our request and parsed it so it treats it as objects not a blob of text+ saved it to a variable 
-        console.log(stationInfoData);
-        var obj = JSON.parse(response);
-        obj.result;
-    });
-    console.log(obj.result);
-
+function getWeatherData(apiData){
+    weatherData = apiData;
+    temperature = weatherData.main.temp;
+    humidity = weatherData.main.humidity;
+    console.log(weatherData);
 }
 
-// function queryAPI(){ //building the url and loading the json in this function 
-//     var query = stationInfoURL + data.stations.station_id.value(); //city.value() lets it take whatever element in the value and link it
-//     loadJSON(query, getWeatherData); //once you've loaded the query, then do this: get weather data
-// }
+function queryAPI(){ //building the url and loading the json in this function 
+    var query = baseURL + city.value() + '&apiKey=' + apiKey + '&units=' + units; //city.value() lets it take whatever element in the value and link it
+    loadJSON(query, getWeatherData); //once you've loaded the query, then do this: get weather data
+}
 
-// function draw(){
-//     background(255); //so it erases everything 
-//     fill(0);
-//     noStroke();
-//     if(stationStatusData){
-//         ellipse(20,20,station_id,station_id * 10)
-//     }
-
-// }
+//*** Draw function ***// 
+function draw(){
+    background(255); //so it erases everything 
+    fill(0);
+    noStroke();
+    // console.log(weatherData); -- if this is in the draw function, it loops over and over again so have it in the getweatherdata function
+    if (weatherData){ //if weatherData is true then draw. f it is undefined don't draw
+        ellipse (200,200, temperature * 10, temperature * 10)
+    }
+}
