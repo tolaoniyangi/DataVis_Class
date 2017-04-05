7452f72d0e48e565e2a3e7d7946ce7e05073e3a1
tolaoniyangi <s.i.oniyangi@gmail.com>
2017-04-05 09:05:12 -0400

    figured out how to load an image onto canvas

--- js/Exercise03.js
+++ js/Exercise03.js
@@ -7,13 +7,16 @@ var weatherData;
 var temperature = 0;
 var humidity = 0;
 var button;
+var img;
 
 //**** Setup function ***//
 function setup(){
-    noCanvas();
+    createCanvas(720,400);
     button = select ('#submit'); //# represents the id of the button 
     city = select ('#city');
     button.mousePressed(queryAPI); //linking queryAPI to the button 
+        img = loadImage("../img/nature.jpg"); 
+
 }
 
 function getWeatherData(apiData){
@@ -22,39 +25,20 @@ function getWeatherData(apiData){
     humidity = weatherData.main.humidity;
     windSpeed=weatherData.wind.speed;
     cityName=weatherData.name;
-    createElement('h1', 'CITY: ' + cityName + '</br></br>TEMPERATURE: ' + temperature + 'C' + '</br></br>HUMIDITY: ' + humidity + '</br></br>WIND SPEED: ' + windSpeed); //h1 is the id, this prints to screen 
+    createElement('h1', 'CITY: ' + cityName + '</br></br>TEMPERATURE: ' + temperature + 'C' + '</br></br>HUMIDITY: ' + humidity + '%' + '</br></br>WIND SPEED: ' + windSpeed + 'mph'); //h1 is the id, this prints to screen 
+// for (var i = 0; i < 400; i++) {
+//     var img = document.createElement("img");    
+//   };
+  // if (humidity > 50) {
+    // img = loadImage("../img/nature.jpg"); 
+  // };
 }
 
 function queryAPI(){ //building the url and loading the json in this function 
-    var query = baseURL + city.value() + '&apiKey=' + apiKey + '&units=' + units; //city.value() lets it take whatever element in the value and link it
-    loadJSON(query, getWeatherData); //once you've loaded the query, then do this: get weather data
+  var query = baseURL + city.value() + '&apiKey=' + apiKey + '&units=' + units; //city.value() lets it take whatever element in the value and link it
+  loadJSON(query, getWeatherData); //once you've loaded the query, then do this: get weather data
 }
 
-// adding google maps
-function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 8,
-    center: {lat: 40.705, lng: -74.258188}
-  });
-  var geocoder = new google.maps.Geocoder();
-
-  document.getElementById('submit').addEventListener('click', function() {
-    geocodeAddress(geocoder, map);
-  });
-}
-
-function geocodeAddress(geocoder, resultsMap) {
-  var address = document.getElementById('city').value; //now linked to previous search box that also does weather query
-  geocoder.geocode({'address': address}, function(results, status) {
-    if (status === 'OK') {
-      resultsMap.setCenter(results[0].geometry.location);
-      var marker = new google.maps.Marker({
-        map: resultsMap,
-        position: results[0].geometry.location
-      });
-    } 
-    else {
-      alert('Geocode was not successful for the following reason: ' + status);
-    }
-  });
+function draw(){
+  image(img, 0, 0);
 }
---
+++ js/Exercise03_Maps.js
@@ -0,0 +1,60 @@
+//**** global variable ****//
+var apiKey = '7db2c379e5eccdbe63af88ebaa03ea48';
+var baseURL = 'http://api.openweathermap.org/data/2.5/weather?q=';
+var city;
+var units = 'metric';
+var weatherData; 
+var temperature = 0;
+var humidity = 0;
+var button;
+
+//**** Setup function ***//
+function setup(){
+    noCanvas();
+    button = select ('#submit'); //# represents the id of the button 
+    city = select ('#city');
+    button.mousePressed(queryAPI); //linking queryAPI to the button 
+}
+
+function getWeatherData(apiData){
+    weatherData = apiData;
+    temperature = weatherData.main.temp;
+    humidity = weatherData.main.humidity;
+    windSpeed=weatherData.wind.speed;
+    cityName=weatherData.name;
+    createElement('h1', 'CITY: ' + cityName + '</br></br>TEMPERATURE: ' + temperature + 'C' + '</br></br>HUMIDITY: ' + humidity + '</br></br>WIND SPEED: ' + windSpeed); //h1 is the id, this prints to screen 
+}
+
+function queryAPI(){ //building the url and loading the json in this function 
+    var query = baseURL + city.value() + '&apiKey=' + apiKey + '&units=' + units; //city.value() lets it take whatever element in the value and link it
+    loadJSON(query, getWeatherData); //once you've loaded the query, then do this: get weather data
+}
+
+// adding google maps
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 8,
+    center: {lat: 40.705, lng: -74.258188}
+  });
+  var geocoder = new google.maps.Geocoder();
+
+  document.getElementById('submit').addEventListener('click', function() {
+    geocodeAddress(geocoder, map);
+  });
+}
+
+function geocodeAddress(geocoder, resultsMap) {
+  var address = document.getElementById('city').value; //now linked to previous search box that also does weather query
+  geocoder.geocode({'address': address}, function(results, status) {
+    if (status === 'OK') {
+      resultsMap.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: resultsMap,
+        position: results[0].geometry.location
+      });
+    } 
+    else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
