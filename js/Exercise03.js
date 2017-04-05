ff30e627b43e0595fbb62db057a7e1bd86577d16
tolaoniyangi <s.i.oniyangi@gmail.com>
2017-04-05 14:58:30 -0400

    Figured out how to get the two images on the canvas at the same time

--- js/Exercise03.js
+++ js/Exercise03.js
@@ -6,10 +6,11 @@ var units = 'metric';
 var weatherData; 
 var temperature = 0;
 var humidity = 0;
+var windSpeed = []; 
 var button;
 var iconRain = [];
 var iconWind = [];
-var canvasRain;
+var canvasHumidity;
 var canvasWind;
 
 function preload(){
@@ -26,70 +27,64 @@ function setup(){
   button = select ('#submit'); //# represents the id of the button 
   city = select ('#city');
   button.mousePressed(queryAPI); //linking queryAPI to the button
-  // createCanvas(800,800);
 }
 
 function getWeatherData(apiData){
   weatherData = apiData;
   temperature = weatherData.main.temp;
   humidity = weatherData.main.humidity;
-  windSpeed=weatherData.wind.speed;
-  cityName=weatherData.name;
+  windSpeed = weatherData.wind.speed;
+  cityName = weatherData.name;
   createElement('h1', 'CITY: ' + cityName + '</br></br>TEMPERATURE: ' + temperature + 'C' + '</br></br>HUMIDITY: ' + humidity + '%' + '</br></br>WIND SPEED: ' + windSpeed + 'mph'); //h1 is the id, this prints to screen 
     
   //drawing the icons 
   // var canvas = createCanvas(800,800);
   // canvas.position(100,150);
-
-
-  //HUMIDITY 
-  var canvasHumidity=createCanvas(200,200);
-  canvasHumidity.position(100,150);
-  h1=createElement('h1', 'HUMIDITY: ' + humidity + '%')
-  h1.position(100,100);
-
-  canvasHumidity.background(0); //so it erases everything 
-    fill(0);
-    noStroke();
-
-  if (humidity >= 75){
-    image(iconRain[0], 0, 0);
-    // subtitle=createElement('p', "Feels like rain")
-  };
-
-  if (humidity >= 50){
-    image(iconRain[1], 0, 0);
-  };
-
-  if (humidity < 50){
-    image(iconRain[2], 0, 0);
-  }
-
-  //WIND 
-  var canvasWind=createCanvas(250,200);
-  canvasWind.position(300,150);
-  h1=createElement('h1', 'WIND SPEED: ' + windSpeed + ' mph')
-  h1.position(300,100);
-
-  canvasWind.background(0); //so it erases everything 
-    fill(0);
-    noStroke();
-
-  if (windSpeed >= 1.5){
-    canvasWind.image(iconWind[0], 0, 0);
-    // subtitle=createElement('p', "Feels like rain")
-  };
-
-  if (windSpeed <= 1.5){
-    canvasWind.image(iconWind[1], 0, 0);
-  };
-
-  if (windSpeed < 0.5){
-    canvasWind.image(iconWind[2], 0, 0);
-  }
 }
 
 function queryAPI(){ //building the url and loading the json in this function 
   var query = baseURL + city.value() + '&apiKey=' + apiKey + '&units=' + units; //city.value() lets it take whatever element in the value and link it
   loadJSON(query, getWeatherData); //once you've loaded the query, then do this: get weather data
+}
+
+function draw(){
+  var canvas=createCanvas(800,800);
+  canvas.position(100,100);
+  canvas.background(255); //so it erases everything 
+    fill(0);
+    noStroke();
+ 
+  // WIND 
+  h1=createElement('h1', 'WIND SPEED: ' + windSpeed + 'mph')
+  h1.position(100,100);
+
+  if (windSpeed >= 1.5){
+    image(iconWind[0], 50, 50);
+    // subtitle=createElement('p', "Feels like rain")
+  };
+
+  if (windSpeed <= 1.5){
+    image(iconWind[1], 50, 50);
+  };
+
+  if (windSpeed < 0.5){
+    image(iconWind[2], 50, 50);
+  }
+
+  //HUMIDITY
+  h1=createElement('h1', 'HUMIDITY: ' + humidity + '%')
+  h1.position(400,100);
+
+  if (humidity >= 75){
+    image(iconRain[0], 300, 50);
+    // subtitle=createElement('p', "Feels like rain")
+  };
+
+  if (humidity >= 50){
+    image(iconRain[1], 300, 50);
+  };
+
+  if (humidity < 50){
+    image(iconRain[2], 300, 50);
+  }
 }
