9aa7b3be9a750d47ac4c6353a7c98ee9a1d68731
tolaoniyangi <s.i.oniyangi@gmail.com>
2017-04-06 13:50:11 -0400

    finished fixing positions and text etc. Gotta figure out how to clear canvas after each mousepressed)

--- js/Exercise03.js
+++ js/Exercise03.js
@@ -45,9 +45,9 @@ function queryAPI(){ //building the url and loading the json in this function
 function draw(){
   var canvas=createCanvas(800,800); //x, y
   canvas.position(150,250);//left, top
-  canvas.background(255); //so it erases everything 
-    fill(0);
-    noStroke();
+  // canvas.background(255); //so it erases everything 
+  //   fill(0);
+  //   noStroke();
   
   //drawing the sketch 
   if(weatherData){ //need this if loop so it only works when there is values for the data 
@@ -62,16 +62,16 @@ function draw(){
     headingTemp.position(500,330);
 
     //CLOUDS 
-    textClouds=createElement ('h3', 'CLOUDS: ' + clouds + '%')
-    textClouds.position(160,160);
+    textClouds=createElement ('h5', 'Clouds: ' + clouds + '%')
+    textClouds.position(500,350);
 
     // WIND icon + text 
-    headingWeather=createElement('h4', 'WIND SPEED: ' + windSpeed + 'm/s')
-    headingWeather.position(100,100);
+    headingWeather=createElement('h5', 'Wind Speed: ' + windSpeed + 'm/s')
+    headingWeather.position(500,380);
 
     //HUMIDITY
-    headingWeather=createElement('h3', 'HUMIDITY: ' + humidity + '%')
-    headingWeather.position(400,100);
+    headingWeather=createElement('h5', 'Humidity: ' + humidity + '%')
+    headingWeather.position(500,410);
 
     if (humidity >= 75 && clouds >= 75){
       image(rainy, 0, 0); //50,50 indicates the position of the image on the screen
@@ -82,31 +82,31 @@ function draw(){
     if (windSpeed < 6.7 && temperature >= 18 && clouds <= 50){
       image(sunny[0], 50, 50);
       subtitle=createElement('h4', "It's beautiful today. Don't forget your sunglasses!") //sundress/shorts + sunglasses
-      subtitle.position(100,100);
+      subtitle.position(500,160);
     };
 
     if (windSpeed < 6.7 && temperature >= 18 && clouds > 50){
       image(sunny[1], 50, 50);
       subtitle=createElement('h4', "It's warm and nice today, although a bit cloudy.") //sundress/shorts 
-      subtitle.position(100,100);
+      subtitle.position(500,160);
     };
 
     if (windSpeed < 6.7 && temperature >= 12 && temperature < 18){
       image(fall, 50, 50);
       subtitle=createElement('h4', "The weather is pretty amazing today - not too hot, not too cold.") //jeans + light sweater 
-      subtitle.position(100,100);
+      subtitle.position(500,160);
     };
 
     if (windSpeed >= 11.2 && humidity < 25 && clouds >= 75 && temperature <= 5){
       image(spring, 50, 50);
       subtitle=createElement('h4', "Feels like spring!") //parka/spring outfit 
-      subtitle.position(100,100);
+      subtitle.position(500,160);
     };
 
     if (windSpeed >= 6.7 && windSpeed < 11.2 && humidity >= 75 && clouds >= 75 && temperature <= 5){
       image(winter, 50, 50);
       subtitle=createElement('h4', "It's cold today - bundle up.") //winter clothes eg northface 
-      subtitle.position(100,100);
+      subtitle.position(500,160);
     };
 
   }
